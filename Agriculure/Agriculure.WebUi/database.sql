b653003c83f1535e4057c80e8673047d514105a0
Ahmed Hamam <ahmedhamam50@gmail.com>
2020-02-26 09:43:58 +0300

    Create database.sql

---
+++ Agriculure/Agriculure.WebUi/database.sql
@@ -0,0 +1,140 @@
+USE [AgricultureII]
+GO
+/****** Object:  Table [dbo].[Contracts]    Script Date: 2/26/2020 9:42:56 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Contracts](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[OfferID] [bigint] NOT NULL,
+	[Quantity] [int] NULL,
+	[Price] [float] NULL,
+	[buyerID] [bigint] NULL,
+	[sellerID] [bigint] NULL,
+	[requestDate] [datetime] NULL,
+	[status] [bit] NULL,
+	[acceptDate] [datetime] NULL,
+ CONSTRAINT [PK_Contracts] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Offer]    Script Date: 2/26/2020 9:42:56 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Offer](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[ProductID] [bigint] NOT NULL,
+	[StartDate] [datetime] NULL,
+	[EndDate] [datetime] NULL,
+	[Status] [bit] NULL,
+	[offerowner] [bigint] NULL,
+	[Descreption] [nvarchar](4000) NULL,
+	[unit] [nvarchar](50) NULL,
+	[quntity] [decimal](18, 2) NULL,
+	[price] [decimal](18, 2) NULL,
+	[currency] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Offer] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 2/26/2020 9:42:56 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[Liecnse] [nvarchar](50) NULL,
+	[UserID] [bigint] NOT NULL,
+	[Description] [nvarchar](4000) NULL,
+	[image] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 2/26/2020 9:42:56 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role](
+	[ID] [nvarchar](50) NOT NULL,
+	[RoleName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 2/26/2020 9:42:56 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NOT NULL,
+	[Address] [nvarchar](200) NULL,
+	[Email] [nvarchar](100) NOT NULL,
+	[RoleID] [nvarchar](50) NOT NULL,
+	[Liecnse] [nvarchar](100) NULL,
+	[NID] [nvarchar](100) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[Phone] [nvarchar](50) NOT NULL,
+	[CompanyName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[Contracts]  WITH CHECK ADD  CONSTRAINT [FK_Contracts_Offer] FOREIGN KEY([OfferID])
+REFERENCES [dbo].[Offer] ([ID])
+GO
+ALTER TABLE [dbo].[Contracts] CHECK CONSTRAINT [FK_Contracts_Offer]
+GO
+ALTER TABLE [dbo].[Contracts]  WITH CHECK ADD  CONSTRAINT [FK_Contracts_Users] FOREIGN KEY([sellerID])
+REFERENCES [dbo].[Users] ([ID])
+GO
+ALTER TABLE [dbo].[Contracts] CHECK CONSTRAINT [FK_Contracts_Users]
+GO
+ALTER TABLE [dbo].[Contracts]  WITH CHECK ADD  CONSTRAINT [FK_Contracts_Users1] FOREIGN KEY([buyerID])
+REFERENCES [dbo].[Users] ([ID])
+GO
+ALTER TABLE [dbo].[Contracts] CHECK CONSTRAINT [FK_Contracts_Users1]
+GO
+ALTER TABLE [dbo].[Offer]  WITH CHECK ADD  CONSTRAINT [FK_Offer_Offer] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ID])
+GO
+ALTER TABLE [dbo].[Offer] CHECK CONSTRAINT [FK_Offer_Offer]
+GO
+ALTER TABLE [dbo].[Offer]  WITH CHECK ADD  CONSTRAINT [FK_Offer_Users] FOREIGN KEY([offerowner])
+REFERENCES [dbo].[Users] ([ID])
+GO
+ALTER TABLE [dbo].[Offer] CHECK CONSTRAINT [FK_Offer_Users]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Users] FOREIGN KEY([UserID])
+REFERENCES [dbo].[Users] ([ID])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Users]
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD  CONSTRAINT [FK_Users_Role] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Role] ([ID])
+GO
+ALTER TABLE [dbo].[Users] CHECK CONSTRAINT [FK_Users_Role]
+GO
