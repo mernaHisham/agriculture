3480f6d27c5b27bbfdee3a8530df97b38a73903d
Ahmed Hamam <ahmedhamam50@gmail.com>
2020-02-26 09:42:28 +0300

    update Database and models

---
+++ Agriculure/Agriculure.WebUi/Models/Offer.cs
@@ -0,0 +1,50 @@
+namespace Agriculure.WebUi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Offer")]
+    public partial class Offer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Offer()
+        {
+            Contracts = new HashSet<Contract>();
+        }
+
+        public long ID { get; set; }
+
+        public long ProductID { get; set; }
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool? Status { get; set; }
+
+        public long? offerowner { get; set; }
+
+        [StringLength(4000)]
+        public string Descreption { get; set; }
+
+        [StringLength(50)]
+        public string unit { get; set; }
+
+        public decimal? quntity { get; set; }
+
+        public decimal? price { get; set; }
+
+        [StringLength(50)]
+        public string currency { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Contract> Contracts { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Agriculure/Agriculure.WebUi/Models/User.cs
@@ -0,0 +1,70 @@
+namespace Agriculure.WebUi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Contracts = new HashSet<Contract>();
+            Contracts1 = new HashSet<Contract>();
+            Offers = new HashSet<Offer>();
+            Products = new HashSet<Product>();
+        }
+
+        public long ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string RoleID { get; set; }
+
+        [StringLength(100)]
+        public string Liecnse { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string NID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CompanyName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Contract> Contracts { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Contract> Contracts1 { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Offer> Offers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Product> Products { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+}
